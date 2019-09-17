98123694278663a33d8713e5c65a14ab970b1752
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-09-17 16:30:50 -0300

    feat(auth) add private route component

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,4 +1,5 @@
 import {Route, Switch} from 'react-router-dom'
+import PrivateRoute from '@/components/auth/PrivateRoute'
 import ErrorPage from './error'
 import HomePage from './home'
 import LoginPage from './login'
@@ -8,6 +9,7 @@ export default function Router() {
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={LoginPage} />
+      <PrivateRoute authorize={Boolean} path="/private" component={HomePage} />
       <Route
         component={() => <ErrorPage statusCode={404} title="Not found" />}
       />
