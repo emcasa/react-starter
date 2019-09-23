4533d6c3fd139e348d31469d93dc24d6f83ef027
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-09-23 10:32:43 -0300

    setup storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import {addDecorator, configure} from '@storybook/react'
 import StoryContainer from './StoryContainer'
+import requireContext from 'require-context.macro'
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../test/components', true, /\.stories\.js$/), module)
+configure(requireContext('../test/components', true, /\.stories\.js$/), module)
 
 addDecorator((story) => <StoryContainer>{story()}</StoryContainer>)
