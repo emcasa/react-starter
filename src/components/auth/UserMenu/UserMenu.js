8e4c3f0161517584d7eebfe64fc7e00789a2551b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-09-23 16:16:23 -0300

    add actions to stories

--- src/components/auth/UserMenu/UserMenu.js
+++ src/components/auth/UserMenu/UserMenu.js
@@ -3,15 +3,19 @@ import Button from '@emcasa/ui-dom/components/Button'
 import Text from '@emcasa/ui-dom/components/Text'
 import Icon from '@emcasa/ui-dom/components/Icon'
 
-export default function UserMenu({user, onLogOut}) {
+export default function UserMenu({color, user, onLogOut}) {
   return (
     <Row alignItems="center">
-      <Text fontSize={18} color="white">
+      <Text fontSize={18} color={color}>
         Hello, {user.name}
       </Text>
       <Button link mrl={2} onClick={onLogOut}>
-        <Icon name="sign-out-alt" color="white" />
+        <Icon name="sign-out-alt" color={color} />
       </Button>
     </Row>
   )
+}
+
+UserMenu.defaultProps = {
+  color: 'white'
 }
--- test/components/auth/Login/UserMenu.stories.js
+++ test/components/auth/Login/UserMenu.stories.js
@@ -1,8 +1,11 @@
+import {action} from '@storybook/addon-actions'
 import UserMenu from '@/components/auth/UserMenu/UserMenu'
 
 export default {
   title: 'auth/UserMenu'
 }
 
-export const userMenu = () => <UserMenu user={{name: 'test'}} />
+export const userMenu = () => (
+  <UserMenu color="pink" user={{name: 'test'}} onLogOut={action('logout')} />
+)
 userMenu.story = {name: 'default'}
