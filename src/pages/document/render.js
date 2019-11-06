553a8efbf6f43facf8b2f79eb0af3d8067e8be4b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-11-05 22:59:54 -0300

    feat(server) handle graphql http status

--- src/pages/document/render.js
+++ src/pages/document/render.js
@@ -1,29 +1,38 @@
 import ReactDOM from 'react-dom/server'
-import {renderToStringWithData} from '@apollo/react-ssr'
+import {getDataFromTree} from '@apollo/react-ssr'
 import {ServerStyleSheet} from 'styled-components'
+import {isGraphQLResponseError, emitGraphQLErrors} from '@/lib/httpStatus'
 import Document from './index'
 
 /**
  * Renders a react element to string
- * @param element      React element to render
- * @param store        Redux store
- * @param apolloClient Apollo client
+ * @param {React.ReactElement} element React element to render
+ * @param {Object} ctx
+ * @param {*} ctx.store                Redux store
+ * @param {*} ctx.apolloClient         Apollo client
  */
-export default async function renderDocument(element, store, apolloClient) {
-  let state = {}
+export default async function renderDocument(
+  element,
+  {store, apolloClient} = {}
+) {
   let styles, markup
   if (element) {
     const styleSheet = new ServerStyleSheet()
     try {
-      markup = await renderToStringWithData(styleSheet.collectStyles(element))
+      await getDataFromTree(element).catch((error) => {
+        if (!isGraphQLResponseError(error)) throw error
+        else emitGraphQLErrors(error)
+      })
+      await store.close()
+      markup = ReactDOM.renderToString(styleSheet.collectStyles(element))
       styles = styleSheet.getStyleElement()
-      state = {
-        redux: store.getState(),
-        apollo: apolloClient.extract()
-      }
     } finally {
       styleSheet.seal()
     }
+  }
+  const state = {
+    redux: store ? store.getState() : undefined,
+    apollo: apolloClient ? apolloClient.extract() : undefined
   }
   const html = ReactDOM.renderToStaticMarkup(
     <Document state={state} styles={styles}>
