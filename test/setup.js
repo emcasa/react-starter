93244f9ddfe7ab9f50d68de451a0afa048877dda
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-07 19:54:08 -0300

    server tests

--- test/setup.js
+++ test/setup.js
@@ -1,16 +1,31 @@
+import path from 'path'
 import {JSDOM} from 'jsdom'
 import chai from 'chai'
 import chaiThings from 'chai-things'
 import Helmet from 'react-helmet'
 
+/*
+ * Global server environment
+ */
+
+process.env.RAZZLE_ASSETS_MANIFEST = path.resolve('./package.json')
+process.env.SSR = false
+
 const dom = new JSDOM('<!doctype html><html><body id="root"></body></html>')
+
+/*
+ * Global browser environment
 
 global.window = dom.window
 global.document = dom.window.document
 global.navigator = {userAgent: 'node.js'}
 
+// Helmet.canUseDom = false
+
+/*
+ * Expectations
+ */
+
 chai.use(chaiThings)
 
 global.should = chai.should()
-
-Helmet.canUseDom = false
