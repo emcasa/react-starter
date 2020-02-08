6a36375ed6c9755f7a895c0e19022d919d8ff712
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-08 01:47:47 -0300

    refactor server tests

--- test/setup/expectations.js
+++
@@ -1,6 +0,0 @@
-import chai from 'chai'
-import chaiThings from 'chai-things'
-
-chai.use(chaiThings)
-
-global.should = chai.should()
--- test/setup/server.env.js
+++ test/setup/server.env.js
@@ -4,4 +4,3 @@ process.env.RAZZLE_ASSETS_MANIFEST = path.resolve(
   __dirname,
   '../fixtures/assets.json'
 )
-process.env.SSR = false
