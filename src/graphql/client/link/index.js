e9cab504218eed9b11e783cd1519520f1acd7e9c
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-11 19:19:06 -0300

    remove ws link

--- src/graphql/client/link/index.js
+++ src/graphql/client/link/index.js
@@ -1,19 +1,7 @@
 import * as ApolloLink from 'apollo-link'
-import {getMainDefinition} from 'apollo-utilities'
 import contextLink from './contextLink'
 import errorLink from './errorLink'
 import httpLink from './httpLink'
-import wsLink from './wsLink'
-
-const isSubscription = ({query}) => {
-  const {kind, operation} = getMainDefinition(query)
-  return kind === 'OperationDefinition' && operation === 'subscription'
-}
-
-const requestLink = (options) =>
-  process.browser
-    ? ApolloLink.split(isSubscription, wsLink(options), httpLink(options))
-    : httpLink(options)
 
 export default function createApolloLink(options) {
   const links = []
@@ -22,7 +10,7 @@ export default function createApolloLink(options) {
   if (process.env.NODE_ENV === 'development' && process.browser)
     links.push(require('apollo-link-logger').default)
   // Terminating link
-  links.push(requestLink(options))
+  links.push(httpLink(options))
 
   return ApolloLink.from(links)
 }
