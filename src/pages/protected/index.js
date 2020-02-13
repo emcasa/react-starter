23c61367fa5b8b19c6b47dc168c048d8f569fa81
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-13 13:04:28 -0300

    remove protected route

--- src/pages/protected/index.js
+++
@@ -1,4 +0,0 @@
-import ProtectedPage from './ProtectedPage'
-import requireAuthentication from '@/lib/hoc/requireAuthentication.js'
-
-export default requireAuthentication(ProtectedPage)
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,7 +2,6 @@ import {Route, Switch} from 'react-router-dom'
 import ErrorBoundary from './error/ErrorBoundary'
 import ErrorPage from './error'
 import HomePage from './home'
-import ProtectedPage from './protected'
 import LoginPage from './login'
 
 const NotFound = () => <ErrorPage emit statusCode={404} message="Not found" />
@@ -13,7 +12,6 @@ export default function Router() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/protected" component={ProtectedPage} />
         <Route component={NotFound} />
       </Switch>
     </ErrorBoundary>
