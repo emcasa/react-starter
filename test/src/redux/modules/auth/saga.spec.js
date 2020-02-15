1b22c7dc2b38d354c87773868554c78f498334fb
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-14 21:37:52 -0300

    remove useless tests

--- test/src/redux/modules/auth/saga.spec.js
+++ test/src/redux/modules/auth/saga.spec.js
@@ -22,52 +22,13 @@ describe('@redux/modules/auth/saga', () => {
     })
   })
 
-  sagaTest(sagas.login).do((it) => {
-    const isTakeAny = (action) => (effect) =>
-      effect.type === 'FORK' && effect.payload.args[0] === action
+  sagaTest(sagas.loginSuccess, 'token').do((it) => {
+    it('persists jwt', (effect) => {
+      effect.should.deep.equal(call(JWT.persist, 'token'))
+    })
 
-    it.forks(
-      'fork EM_CASA_SUBMIT_TOKEN',
-      isTakeAny(TYPES.EM_CASA_SUBMIT_TOKEN),
-      (it) => {
-        const jwt = 'test_jwt'
-        const user = {id: 123}
-
-        /**
-         *  Progress @emcasa/login's saga
-         */
-
-        it('takes an EM_CASA_SUBMIT_TOKEN', () => ({
-          type: TYPES.EM_CASA_SUBMIT_TOKEN,
-          token: 'test_token',
-          promiseDispatcher: {resolve: jest.fn(), reject: jest.fn()}
-        }))
-
-        it.forks('forks to @emcasa/login`s submitToken saga', () => true)
-
-        it('submits api token', ({payload}) => {
-          payload.fn.should.equal(apolloClient.mutate)
-          return {
-            data: {signInVerifyAuthenticationCode: {jwt, user}}
-          }
-        })
-
-        /**
-         *  Actually test loginSuccess saga
-         */
-
-        it.forks(call(sagas.loginSuccess, jwt, user))
-
-        it('persists jwt', (effect) => {
-          effect.should.deep.equal(call(JWT.persist, jwt))
-        })
-
-        it('resets the graphql store', (effect) => {
-          effect.should.deep.equal(
-            call([apolloClient, apolloClient.resetStore])
-          )
-        })
-      }
-    )
+    it('resets the graphql store', (effect) => {
+      effect.should.deep.equal(call([apolloClient, apolloClient.resetStore]))
+    })
   })
 })
