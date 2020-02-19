2eb79046115c155cadd0bf6d6e63e0ab1c88d759
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-18 23:58:33 -0300

    persistent apollo cache

--- src/graphql/client/index.js
+++ src/graphql/client/index.js
@@ -1,4 +1,5 @@
 import {ApolloClient} from 'apollo-client'
+import {CachePersistor} from 'apollo-cache-persist'
 import {InMemoryCache, defaultDataIdFromObject} from 'apollo-cache-inmemory'
 import {getToken} from '@/lib/jwt'
 import initialState from '../resolvers/initialState'
@@ -7,18 +8,40 @@ import createLink from './link'
 
 let apolloClient = null
 
+const SCHEMA_VERSION = '1'
+
 const def = (options) => ({
   getToken,
   ...options
 })
+
+async function persistCache(cache, state) {
+  if (state) cache.restore(state)
+  else cache.writeData({data: initialState})
+
+  if (!process.browser) return
+
+  const persistor = new CachePersistor({
+    cache,
+    storage: window.localStorage,
+    debug: process.env.NODE_ENV === 'development'
+  })
+
+  const currentVersion = window.localStorage.getItem('__VERSION__')
+
+  if (currentVersion === SCHEMA_VERSION) await persistor.restore()
+  else {
+    await persistor.purge()
+    window.localStorage.setItem('__VERSION__', SCHEMA_VERSION)
+  }
+}
 
 function createApolloClient(options, state) {
   const cache = new InMemoryCache({
     dataIdFromObject: (data) =>
       data.uuid ? data.uuid : defaultDataIdFromObject(data)
   })
-  if (state) cache.restore(state)
-  else cache.writeData({data: initialState})
+  persistCache(cache, state)
   const client = new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser,
